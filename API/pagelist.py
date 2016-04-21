0f6c0247a63fe81f9812afe07df7a8d8a65f2c69
YOUR NAMEMandy <mz962@mail.missouri.edu>
2016-04-21 12:48:26 -0500

    add notes in python file

--- API/pagelist.py
+++ API/pagelist.py
@@ -12,6 +12,8 @@ response = urllib2.urlopen('http://openstates.org/api/v1/bills/?apikey=1f366e071
 
 bills = json.loads(response)
 
+
+# Set up a list, in order to put all Json code into a list (after read each bill_detail page)
 bill_re = []
 
 for bill in bills:
@@ -20,20 +22,25 @@ for bill in bills:
     # a bill ID (which has spaces in it)through a URL in Python, you need to do this first.
     encoded_bill_id = urllib.quote(bill['bill_id'])
 
+    # Put a list of bill id into url
     url = 'http://openstates.org/api/v1/bills/mo/2016/'+encoded_bill_id+'/?apikey=1f366e0712bd4ad6b079afe3bb993434'
 
+    # Read each bill_detail page
     bill_page = urllib2.urlopen(url).read()
 
     bill_detail = json.loads(bill_page)
 
     bill_re.append(bill_detail)
 
+    
+    # Get sponsor, title, id from each page
     for bill_detail in bill_re:
 
         sponsor_detail = bill_detail['sponsors']
 
         for sponsors in sponsor_detail:
 
+            # Remember to encode('utf-8'), get rid of "u"
             sponsor_name = sponsors['name'].encode('utf-8')
             title_name = bill_detail['title'].encode('utf-8')
             official_title = bill_detail['bill_id'].encode('utf-8')
