6f56415684c5a6ec4b0052910d8fce0ec23008af
YOUR NAMEMandy <mz962@mail.missouri.edu>
2016-04-21 12:43:31 -0500

    API assignment

---
+++ API/pagelist.py
@@ -0,0 +1,42 @@
+import urllib2, json, urllib,csv
+import urlparse
+from urllib import urlencode
+
+output = open('ApiList.csv','w')
+writer = csv.writer(output, delimiter = ",")
+
+writer.writerow(['bill_id','primary sponsor','bill_title'])
+
+
+response = urllib2.urlopen('http://openstates.org/api/v1/bills/?apikey=1f366e0712bd4ad6b079afe3bb993434&state=mo&search_window=session').read()
+
+bills = json.loads(response)
+
+bill_re = []
+
+for bill in bills:
+
+    # This is the processing piece I mentioned in the assignment description. To include
+    # a bill ID (which has spaces in it)through a URL in Python, you need to do this first.
+    encoded_bill_id = urllib.quote(bill['bill_id'])
+
+    url = 'http://openstates.org/api/v1/bills/mo/2016/'+encoded_bill_id+'/?apikey=1f366e0712bd4ad6b079afe3bb993434'
+
+    bill_page = urllib2.urlopen(url).read()
+
+    bill_detail = json.loads(bill_page)
+
+    bill_re.append(bill_detail)
+
+    for bill_detail in bill_re:
+
+        sponsor_detail = bill_detail['sponsors']
+
+        for sponsors in sponsor_detail:
+
+            sponsor_name = sponsors['name'].encode('utf-8')
+            title_name = bill_detail['title'].encode('utf-8')
+            official_title = bill_detail['bill_id'].encode('utf-8')
+
+    # print official_title, sponsor_name, title_name
+    writer.writerow([official_title, sponsor_name, title_name])
